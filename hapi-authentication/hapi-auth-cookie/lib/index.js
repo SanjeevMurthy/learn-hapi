3b1d230a5e23f12dd14551c6fa2a6ad737a0241a
sanjeevmurthy <sanjeevmurthy01@gmail.com>
2017-03-05 23:28:06 +0530

    hapi cookie based athentication

---
+++ hapi-authentication/hapi-auth-cookie/lib/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Cookie = require('hapi-auth-cookie');
+const Blipp = require('blipp');
+const routes = require('./routes');
+
+const server = new Hapi.Server();
+server.connection({ host: '127.0.0.1', port: 1337 });
+
+server.register([
+    Cookie,
+    { register: Blipp, options: { showAuth: true } }
+], (err) => {
+
+    // handle err logic
+
+    server.auth.strategy(
+        'session',
+        'cookie',
+        {
+            cookie: 'example',
+            password: 'password-must-be-at-least-32-characters',
+            isSecure: false,
+            redirectTo: '/login',
+            redirectOnTry: false
+        }
+    );
+    server.auth.default('session');
+
+    server.route(routes);
+
+    server.start((err) => {
+
+        console.log(`Server running at ${server.info.uri}`);
+    });
+});
